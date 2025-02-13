011d09bc1bd763909f5c6152ce1fb2772cac6026
ryohei <ryohei.shibasaki+dev@gmail.com>
2025-02-13 21:32:57 +0900

    add init.el

---
+++ init.el
@@ -0,0 +1,8 @@
+
+(global-set-key "\C-h" 'backword-char)
+(global-set-key "\C-j" 'next-line)
+(global-set-key "\C-k" 'previous-line)
+(global-set-key "\C-l" 'forword-char)
+
+;(global-set-key "C-n" 'newline-adn-indent)
+;(global-set-key "\C-o 'kill-line")
